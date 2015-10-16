b411e1b2841e7dadc03229b0b94566ff917c705a
root <shaun@omeara.co.za>
2015-10-16 08:24:07 -0700

    Update hiera to hiera and tasks

--- deployment_scripts/network_hiera_override.pp
+++ deployment_scripts/network_hiera_override.pp
@@ -1,36 +1,61 @@
 notice('MODULAR: network-node/network_hiera_override.pp')
 
-$network_node_plugin = hiera('network=node', undef)
+$network_node_plugin = hiera('network-node', undef)
 $hiera_dir = '/etc/hiera/override'
 $plugin_name = 'network-node'
 $plugin_yaml = "${plugin_name}.yaml"
 
 
-if $network_node_plugin {
-  
-  $corosync_roles = ['primary-network-node', 'network-node']
-  $haproxy_nodes = false
-  $quantum_settings['neutron_agents'] = ['l3', 'metadata', 'dhcp']
-  $quantum_settings["neutron_server_enable"] = false
-  $quantum_settings["conf_nova"] = false
 
- ###################
+if $network_node_plugin {
+  $network_metadata = hiera_hash('network_metadata')
+  $network_roles = ['primary-network-node', 'network-node']
+  $network_nodes = get_nodes_hash_by_roles($network_metadata, $network_roles)
+ 
+  case hiera_array('role', 'none') {
+    /primary-network-node/: {  
+       $corosync_roles = $network_roles
+       $deploy_vrouter = false
+       $haproxy_nodes = false
+       $corosync_nodes = $network_nodes
+    	}
+	}
+
+###################
+$calculated_content = inline_template('
+<% if @corosync_nodes -%>
+<% require "yaml" -%>
+corosync_nodes:
+<%= YAML.dump(@corosync_nodes).sub(/--- *$/,"") %>
+<% end -%>
+<% if @corosync_roles -%>
+corosync_roles:
+<%
+@corosync_roles.each do |crole|
+%>  - <%= crole %>
+<% end -%>
+<% end -%>
+deploy_vrouter: <%= @deploy_vrouter %>
+')
+
+###################
+
   file {'/etc/hiera/override':
     ensure  => directory,
   } ->
-  file { "${hiera_dir}/${plugin_yaml}":
+  file { '/etc/hiera/override/common.yaml':
     ensure  => file,
-    content => "${network_node_plugin['yaml_additional_config']}\n${calculated_content}\n",
+    content => "${calculated_content}\n",
   }
-
+  
   package {'ruby-deep-merge':
     ensure  => 'installed',
-  }
-
-  file_line {"${plugin_name}_hiera_override":
+  } 
+  
+  file_line {'hiera.yaml':
     path  => '/etc/hiera.yaml',
-    line  => "  - override/${plugin_name}",
-    after => '  - override/module/%{calling_module}',
+      line  => "  - override/${plugin_name}",
+      after => '  - override/module/%{calling_module}',
   }
 
 }
